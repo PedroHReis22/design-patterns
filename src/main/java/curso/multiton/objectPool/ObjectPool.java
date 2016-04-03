b48c432a50164418600361fecad3ba6cd3152cea
Pedro Henrique Reis <pedro.hreis22@gmail.com>
2016-04-03 18:00:20 -0300

    Padrão Object Pool

---
+++ src/main/java/curso/multiton/objectPool/ObjectPool.java
@@ -0,0 +1,72 @@
+package curso.multiton.objectPool;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public abstract class ObjectPool<T> {
+	
+	protected long expirationTime;
+	private Hashtable<T, Long> locked, unlocked;
+	
+	public ObjectPool() {
+		expirationTime = 15000;
+		locked = new Hashtable<>();
+		unlocked = new Hashtable<>();
+	}
+	
+	protected abstract T create();
+
+	public abstract boolean validate(T t);
+	
+	public abstract void expire(T t);
+	
+	public synchronized T acquire() {
+		
+		T t;
+		
+		long now = System.currentTimeMillis();
+		
+		if(unlocked.size() > 0) {
+			
+			Enumeration<T> e = unlocked.keys();
+			
+			while(e.hasMoreElements()) {
+				
+				t = e.nextElement();
+				
+				if((now - unlocked.get(t)) > expirationTime) { //objeto expirado
+					unlocked.remove(t);
+					expire(t);
+					t = null;
+				}
+				else if(validate(t)){
+					unlocked.remove(t);
+					locked.put(t, now);
+					return t;
+				}
+				else {
+					unlocked.remove(t);
+					expire(t);
+					t = null;
+				}
+				
+			}
+			
+		}
+		
+		t = create();
+		locked.put(t, now);
+		
+		return t;
+		
+	}
+	
+	public synchronized void release(T t) {
+		unlocked.put(t, locked.remove(t));
+	}
+	
+	public long getExpirationTime() {
+		return expirationTime;
+	}
+	
+}
---
+++ src/test/java/curso/ConnectionPoolTest.java
@@ -0,0 +1,52 @@
+package curso;
+
+import java.sql.Connection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import curso.multiton.objectPool.ConnectionPool;
+
+public class ConnectionPoolTest {
+	
+	@Test
+	public void testingSameConnection() {
+		
+		ConnectionPool connectionPool = 
+				new ConnectionPool("com.mysql.jdbc.Driver", 
+						"jdbc:mysql://localhost:3306", "root", "pedro2201");
+		
+		Connection connection1 = connectionPool.acquire();
+		//usa conexão
+		connectionPool.release(connection1);
+		
+		try { Thread.sleep(connectionPool.getExpirationTime() / 2); }
+		catch (InterruptedException e) { e.printStackTrace(); }
+		
+		Connection connection2 = connectionPool.acquire();
+		
+		Assert.assertSame("Testando a mesma conexão", connection1, connection2);
+		
+	}
+	
+	@Test
+	public void testingDiffConnections() {
+		
+		ConnectionPool connectionPool = 
+				new ConnectionPool("com.mysql.jdbc.Driver", 
+						"jdbc:mysql://localhost:3306", "root", "pedro2201");
+		
+		Connection connection1 = connectionPool.acquire();
+		//usa conexão
+		connectionPool.release(connection1);
+		
+		try { Thread.sleep(connectionPool.getExpirationTime()); }
+		catch (InterruptedException e) { e.printStackTrace(); }
+		
+		Connection connection2 = connectionPool.acquire();
+		
+		Assert.assertNotSame("Testando conexões diferentes", connection1, connection2);
+		
+	}
+
+}
